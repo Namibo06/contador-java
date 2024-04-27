57d2e0e05d0ad8ef5aaaf9a977f302f64c9705ae
Namibo06 <namibo654@gmail.com>
2024-04-27 13:02:43 -0300

    perf: Contador

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm,parametroDois);
+        }catch (ParametrosInvalidosException e){
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int paramentroUm,int parametroDois) throws ParametrosInvalidosException{
+        if(paramentroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - paramentroUm;
+
+        for (int i=0;i <= contagem;i++){
+            System.out.println(i);
+        }
+    }
 }
